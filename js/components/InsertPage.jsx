c4d002f64f655fe1af047fbf2d24737e7a169983
Mihnea Mares <mihneaflorian13@gmail.com>
2023-05-16 01:23:49 +0300

    done front

---
+++ js/components/InsertPage.jsx
@@ -0,0 +1,57 @@
+// js/components/InsertPage.jsx
+export default function InsertPage() {
+	const insertRecord = (event) => {
+		event.preventDefault();
+		const title = document.getElementById("title").value;
+		const author = document.getElementById("author").value;
+		const release_date = document.getElementById("release_date").value;
+		const data = {title, author, release_date};
+		fetch("/api/records", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		}).then(() => {
+			console.log("New record inserted");
+			document.getElementById("title").value = "";
+			document.getElementById("author").value = "";
+			document.getElementById("release_date").value = "";
+		});
+	}
+
+	return (
+		<section className="bg-white dark:bg-gray-900">
+			<div className="container px-6 py-10 mx-auto">
+				<h1 className="w-[500px] mx-auto text-center text-6xl">Wish Books Hub</h1>
+
+				<form>
+					<div className="mb-6">
+						<label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Book title</label>
+						<input type="text" id="title"
+						       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						       placeholder="Book title" required/>
+					</div>
+					<div className="mb-6">
+						<label htmlFor="author"
+						       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author</label>
+						<input type="text" id="author"
+						       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						       placeholder="Author" required/>
+					</div>
+					<div>
+						<label htmlFor="release_date"
+						       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Release Year</label>
+						<input type="text" id="release_date"
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								placeholder="Release year" required/>
+					</div>
+					<button type="submit"
+					        onClick={ insertRecord }
+					        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+					</button>
+				</form>
+			</div>
+		</section>
+	)
+}
---
+++ js/components/MainPage.jsx
@@ -0,0 +1,69 @@
+// js/components/MainPage.jsx
+import {useEffect, useState} from "react";
+
+export default function MainPage() {
+	const [records, setRecords] = useState([]);
+
+	useEffect(() => {
+		try{
+			fetch('/api/records', {
+				method: 'GET',
+			})
+				.then(response => response.json())
+				.then(json => setRecords(json.data));
+		}
+		catch (error) {
+			console.log(error);
+		}
+	}, []);
+
+	const deleteRecord = (event) => {
+		event.preventDefault();
+		const id = event.target.id;
+		try {
+			fetch(`/api/records?id=${id}`, {
+				method: 'DELETE',
+			})
+				.then(response => response.json())
+				.then(json => {
+						setRecords(records.filter(record => record._id !== id));
+				});
+		}
+		catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<section className="bg-gray-200">
+			<div className="container px-6 py-10 mx-auto">
+				<h1 className="mx-auto text-center text-6xl text-purple-500">Wish Books Hub</h1>
+
+				<div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+					{records.map(record => (
+						<div
+							key={record._id}
+							className="block max-w-sm p-6 bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+							<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+								{record.title}
+							</h5>
+							<p className="font-normal text-gray-700 text-gray-400">
+								Author: {record.author}
+							</p>
+                            <p className="font-normal text-gray-700 text-gray-400">
+								Release year: {record.release_date}
+							</p>
+							<div className={"flex justify-center mt-4"}>
+								<button type="button"
+								        id={record._id}
+								        onClick={deleteRecord}
+								        className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+	)
+}
